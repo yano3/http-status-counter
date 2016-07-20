5519ac5f7f4f6b9b18051a9523b76ec2d92aac47
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-07-20 12:51:26 +0900

    initialize to stats values and reduce type conversions

--- status_counter/status_counter_init.rb
+++ status_counter/status_counter_init.rb
@@ -8,10 +8,11 @@ class StatusCounter
   def update
     v = Server::Var.new
     status = v.status.to_s
-
     stats = current_stats
 
-    stats[status.to_sym] = (stats[status.to_sym].to_i + 1).to_s
+    stats[status.to_sym] ||= 0
+    stats[status.to_sym] = stats[status.to_sym] + 1
+
     @cache["stats"] = stats.to_s
   end
 

bba716904b2288f1d80cf99e3aa88debb8e158c0
Teppei Yano <buckelwal@gmail.com>
2016-10-28 18:33:23 +0900

    calc average request time

--- status_counter/status_counter_init.rb
+++ status_counter/status_counter_init.rb
@@ -11,6 +11,7 @@ Userdata.new.shared_cache = Cache.new config
 class StatusCounter
   def initialize c
     @cache = c
+    @interval = 60
   end
 
   def update
@@ -32,7 +33,37 @@ class StatusCounter
     request_time = (v.request_time.to_f * 1000).to_i
     stats[:request_time] += request_time
 
+    if @cache["period_statistics"]
+      period_statistics = eval(@cache["period_statistics"])
+    else
+      period_statistics = init_period_statistics
+    end
+
+    # calc average response time
+    if Time.now.to_i - period_statistics[:updated] > @interval
+      avg_request_time = nil
+      if period_statistics[:requests].to_i > 0
+        avg_request_time = period_statistics[:request_time] / period_statistics[:requests]
+      end
+
+      stats[:avg_request_time] = avg_request_time
+      period_statistics[:updated] = Time.now.to_i
+      period_statistics = init_period_statistics
+    end
+
+    period_statistics[:requests] += 1
+    period_statistics[:request_time] += request_time
+
+    @cache["period_statistics"] = period_statistics.to_s
     @cache["stats"] = stats.to_s
+  end
+
+  def init_period_statistics
+    {
+      requests: 0,
+      request_time: 0,
+      updated: Time.now.to_i,
+    }
   end
 
   def output
@@ -50,6 +81,7 @@ class StatusCounter
         status: { },
         body_bytes_sent: 0,
         request_time: 0,
+        avg_request_time: nil,
       }
     end
   end

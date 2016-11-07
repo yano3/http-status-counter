eeb53901d1b6c70cf472bb0a5164ba7e060bb308
Teppei Yano <buckelwal@gmail.com>
2016-11-07 18:54:48 +0900

    support upstream_response_time

--- status_counter/status_counter_init.rb
+++ status_counter/status_counter_init.rb
@@ -32,14 +32,19 @@ class StatusCounter
 
     request_time = (v.request_time.to_f * 1000).to_i
     stats[:request_time] += request_time
+    upstream_response_time = (v.upstream_response_time.to_f * 1000).to_i
+    stats[:upstream_response_time] += upstream_response_time
 
-    # calc average response time
+    # calculate latency
     if Time.now.to_i - stats[:period_updated] > @interval
       if stats[:period_requests].to_i > 0
         avg_request_time = stats[:period_request_time] / stats[:period_requests]
+        avg_upstream_response_time = stats[:period_upstream_response_time] / stats[:period_requests]
         stats[:avg_request_time] = avg_request_time
+        stats[:avg_upstream_response_time] = avg_upstream_response_time
         stats[:period_requests] = 0
         stats[:period_request_time] = 0
+        stats[:period_upstream_response_time] = 0
         stats[:period_updated] = Time.now.to_i
       end
     end
@@ -47,6 +52,7 @@ class StatusCounter
     if status_code.to_i >= 200 && status_code.to_i < 300
       stats[:period_requests] += 1
       stats[:period_request_time] += request_time
+      stats[:period_upstream_response_time] += upstream_response_time
     end
 
     @cache["stats"] = stats.to_s
@@ -65,7 +71,9 @@ class StatusCounter
       status: stats[:status],
       body_bytes_sent: stats[:body_bytes_sent],
       request_time: stats[:request_time],
-      avg_request_time: stats[:avg_request_time]
+      avg_request_time: stats[:avg_request_time],
+      upstream_response_time: stats[:upstream_response_time],
+      avg_upstream_response_time: stats[:avg_upstream_response_time]
     }
 
     Server.echo JSON::stringify(out)
@@ -80,9 +88,11 @@ class StatusCounter
         status: { },
         body_bytes_sent: 0,
         request_time: 0,
+        upstream_response_time: 0,
         avg_request_time: nil,
         period_requests: 0,
         period_request_time: 0,
+        period_upstream_response_time: 0,
         period_updated: Time.now.to_i
       }
     end
